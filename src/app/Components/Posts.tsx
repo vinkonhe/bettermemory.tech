024a4e85621fbbbc8377b81f62d532090beeb749
vinkonhe <vinkonhe@gmail.com>
2025-03-29 20:33:12 +0800

    update:Posts.tsx

--- src/app/Components/Posts.tsx
+++ src/app/Components/Posts.tsx
@@ -11,11 +11,13 @@ export default function Posts({ title, date, to }: PostsProps) {
     <div className="flex flex-col ">
       <a
         href={`blog/${to}`}
-        className="cursor-pointer text-[16px] mt-5 opacity-85 hover:opacity-100 underline hover:decoration-2"
+        className="cursor-pointer text-[16px] mt-5 opacity-85 hover:opacity-100 underline hover:decoration-2 dark:text-black"
       >
         {title}
       </a>
-      <a className="cursor-default text-[12px] mt-2 opacity-85">{date}</a>
+      <a className="cursor-default text-[12px] mt-2 opacity-85 dark:text-black">
+        {date}
+      </a>
     </div>
   );
 }
