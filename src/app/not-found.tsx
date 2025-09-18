b7270a3ee0a479f7e1adde945b929c1eb707cdc9
vinkonhe <vinkonhe@gmail.com>
2025-09-19 07:54:44 +0800

    update:404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,15 @@
 export default function NotFound() {
-  return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        404 - Page Not Found
-      </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
-    </section>
-  )
+    return (
+        <section>
+            <div className="flex flex-col pt-[32px]">
+                <a className="cursor-default pt-[16px] text-[16px] font-semibold text-[#0C0A09] selection:bg-black selection:text-[#FFFFFF]">
+                    Page Not Found
+                </a>
+                <a className="cursor-default pt-[8px] text-[16px] font-normal text-[#44403C] selection:bg-black selection:text-[#FFFFFF]">
+                    The page you’re looking for doesn’t exist. Please check the
+                    URL or return to the homepage using the navigation below.
+                </a>
+            </div>
+        </section>
+    );
 }
